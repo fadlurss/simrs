21b570ee20228e46c8f27ab3b41bae5c1a5417ed
fadlurss <jhieber7@gmail.com>
2019-03-14 18:20:45 +0700

    add env

--- config/database.js
+++ config/database.js
@@ -1,8 +1,8 @@
 // config/database.js
 module.exports = {
 
-    'url': 'mongodb://localhost/simrs',
-    // 'url' : process.env.DATABASEURL,
+    // 'url': 'mongodb://localhost/simrs',
+    'url': process.env.DATABASEURL,
     'secret': "jdbvdsvjbsfew7843grbfd" // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 
 };
