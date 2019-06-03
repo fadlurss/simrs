5eccd628254e52ff43f519233ac3d228058cef49
fadlurss <jhieber7@gmail.com>
2019-06-03 13:41:10 +0700

    routes user

--- config/database.js
+++ config/database.js
@@ -1,7 +1,9 @@
 // config/database.js
 module.exports = {
 
+    // 'url': 'mongodb://fadlurss:Xtcbandung97@ds213896.mlab.com:13896/simrs',
     'url': 'mongodb+srv://fadlurs:xtcbandung@simrscluster-awvwj.mongodb.net/test?retryWrites=true&w=majority',
     'secret': 'dgggfgfg'
-    // mongodb://fadlurss:Xtcbandung97@ds213896.mlab.com:13896/simrs
+    // mongodb+srv://fadlurs:xtcbandung@simrscluster-awvwj.mongodb.net/test?retryWrites=true&w=majority
+    // 
 };
